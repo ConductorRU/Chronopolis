3ecc7cc761db5248eb6c776b22ad08ba615184c5
Conductor <draaks@yandex.ru>
2017-06-28 22:13:48 +0500

    Web

---
+++ chronopolis.ru/pch.h
@@ -0,0 +1,16 @@
+﻿//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+#include <collection.h>
+#include <agents.h>
+#include <ppl.h>
+#include <ppltasks.h>
+#include <array>
+#include <iostream>
+#include "App.xaml.h"
+using namespace concurrency;
+using namespace std;
